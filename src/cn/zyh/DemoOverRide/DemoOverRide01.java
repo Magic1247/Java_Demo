aece8fdcf68958df8cc1ca0701f5c0aef52a6d0c
Huaiguang <1533505111@qq.com>
2020-05-25 20:52:12 +0800

    TestArraylist;

---
+++ src/cn/zyh/DemoOverRide/DemoOverRide01.java
@@ -0,0 +1,9 @@
+package cn.zyh.DemoOverRide;
+
+public class DemoOverRide01 {
+    public static void main(String[] args) {
+        Zi zi = new Zi();
+        zi.method();
+    }
+
+}
---
+++ src/cn/zyh/DemoOverRide/Fu.java
@@ -0,0 +1,7 @@
+package cn.zyh.DemoOverRide;
+
+public class Fu {
+    public void method(){
+        System.out.println("父类方法执行！");
+    }
+}
