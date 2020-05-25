a1431a89ae52d29c20831cc4b223d31c484b95e0
Huaiguang <1533505111@qq.com>
2020-05-25 21:16:06 +0800

    TestExtends

---
+++ src/cn/zyh/DemoPhoneExtends/DemoPhoneExtends01.java
@@ -0,0 +1,15 @@
+package cn.zyh.DemoPhoneExtends;
+
+public class DemoPhoneExtends01 {
+    public static void main(String[] args) {
+        Phone phone = new Phone();
+        phone.call();
+        phone.sendMessage();
+        phone.show();
+        System.out.println("================");
+        NewPhone iPhone = new NewPhone();
+        iPhone.call();
+        iPhone.sendMessage();
+        iPhone.show();
+    }
+}
