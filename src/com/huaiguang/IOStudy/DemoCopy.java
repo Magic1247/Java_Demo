3910715d997dc0af5ce3541fab953ff2c2bce45a
Huaiguang <1533505111@qq.com>
2021-05-26 01:16:51 +0800

    demoCopy

--- src/com/huaiguang/IOStudy/DemoCopy.java
+++ src/com/huaiguang/IOStudy/DemoCopy.java
@@ -8,14 +8,14 @@ public class DemoCopy {
     }
 
     private static void copy(String sFile,String eFile) throws IOException {  // 输入源文件及复制的目的地
-        InputStream fis = new FileInputStream(sFile);
-        byte[] bytes = new byte[10240];
-        int len = 0;
-        while ((len = fis.read(bytes)) != -1 ){
+        InputStream fis = new FileInputStream(sFile); // 多态创建文件输入流，读取源文件
+        byte[] bytes = new byte[10240];               // 创建字节数组
+        int len = 0;                                  // 定义初始值
+        while ((len = fis.read(bytes)) != -1 ){       // 文件结束标识jvm返回-1,将read()返回值赋值给len, 当read()返回不为-1时循环读取文件
             System.out.println(new String(bytes,0,len));     // 数组转string，offset表示开始转换的索引，len表示转换的个数
         }
-        OutputStream fos = new FileOutputStream(eFile);
-        fos.write(bytes);
+        OutputStream fos = new FileOutputStream(eFile);  // 创建文件输出流。写入文件
+        fos.write(bytes);                                // 将读取到的字节数组写入复制目的地
         System.out.println("Succeeded Copy");
     }
 }
