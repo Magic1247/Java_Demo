e481650813c53b36b304ab777587dc69608a8606
Huaiguang <1533505111@qq.com>
2021-06-27 17:36:36 +0800

    StreamStudy

---
+++ src/com/huaiguang/StreamStudy/DemoList.java
@@ -0,0 +1,33 @@
+package com.huaiguang.StreamStudy;
+
+import java.util.ArrayList;
+
+public class DemoList {
+    public static void main(String[] args) {
+        demo01();
+    }
+
+    private static void demo01() {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("张无忌");
+        list.add("张无忌1");
+        list.add("张无忌2");
+        list.add("张无忌3");
+        list.add("无忌");
+        System.out.println(list);
+        ArrayList<String> ZhangList = new ArrayList<>();
+        for (String s : list){
+            if(s.startsWith("张")){
+                ZhangList.add(s);
+            }
+        }
+        System.out.println(ZhangList);
+        ArrayList<String> tlist = new ArrayList<>();
+        for (String s : ZhangList){
+            if(s.length() == 3){
+                tlist.add(s);
+            }
+        }
+        System.out.println(tlist);
+    }
+}
