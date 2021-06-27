76bc169a0f2bda046761b45f0c008478fd81aac4
Huaiguang <1533505111@qq.com>
2021-06-28 00:10:27 +0800

    FunctionInterfaceStudy

---
+++ src/com/huaiguang/FunctionInterfaceStudy/DemoLambda.java
@@ -0,0 +1,18 @@
+package com.huaiguang.FunctionInterfaceStudy;
+
+public class DemoLambda {
+    public static void main(String[] args) {
+        StartThread(()->{
+            System.out.println(Thread.currentThread().getName()+"Lambda线程开启");
+        });
+        StartThread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println(Thread.currentThread().getName()+"匿名内部类线程开启");
+            }
+        });
+    }
+    public static void StartThread(Runnable run){
+        new Thread(run).start();
+    }
+}
