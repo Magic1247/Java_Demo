f388aa78bda971d672fb866341669c2df5315a00
Huaiguang <1533505111@qq.com>
2021-07-11 23:44:33 +0800

    FunctionInterfaceStudy

--- src/com/huaiguang/FunctionInterfaceStudy/DemoGetMax.java
+++ src/com/huaiguang/FunctionInterfaceStudy/DemoGetMax.java
@@ -1,19 +1,26 @@
 package com.huaiguang.FunctionInterfaceStudy;
 
+import java.util.function.Consumer;
 import java.util.function.Supplier;
 
 public class DemoGetMax {
     public static void main(String[] args) {
-        int[] arr = {100,1,300};
-        System.out.println(getMax(() -> {
-            int max = arr[0];
-            for (int i : arr) {
-                if (i > max) {
-                    max = i;
-                }
-            }
-            return max;
-        }));
+//        int[] arr = {100,1,300};
+//        System.out.println(getMax(() -> {
+//            int max = arr[0];
+//            for (int i : arr) {
+//                if (i > max) {
+//                    max = i;
+//                }
+//            }
+//            return max;
+//        }));
+        con("蔡徐坤",(name) -> {
+            System.out.println("消费"+name);
+        });
+    }
+    private static void con(String name, Consumer<String> con){
+        con.accept(name);
     }
 
     private static int getMax(Supplier<Integer> integerSupplier) {
