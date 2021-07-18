07485ed17b1971f176c2dd81f0e08cc82b816994
Huaiguang <1533505111@qq.com>
2021-07-19 00:03:11 +0800

    FunctionInterfaceStudy0718

--- src/com/huaiguang/FunctionInterfaceStudy/DemoFun.java
+++ src/com/huaiguang/FunctionInterfaceStudy/DemoFun.java
@@ -1,13 +1,24 @@
 package com.huaiguang.FunctionInterfaceStudy;
 
+
 import java.util.function.Function;
 
 public class DemoFun {
     public static void change(String str, Function<String,Integer> fun){
         System.out.println(fun.apply(str));
     }
+    public static void changeAndThen(String str, Function<String,Integer> fun1, Function<Integer, String> fun2){
+        System.out.println(fun1.andThen(fun2).apply(str));
+    }
 
     public static void main(String[] args) {
-        change("123456",s -> Integer.parseInt(s));
+//        change("123456",s -> Integer.parseInt(s));
+        changeAndThen("100",
+                s -> Integer.parseInt(s),
+                s -> {
+                    s+=10;
+                    return s.toString();}
+                );
     }
+
 }
---
+++ src/com/huaiguang/FunctionInterfaceStudy/FinalDemoFun.java
@@ -0,0 +1,17 @@
+package com.huaiguang.FunctionInterfaceStudy;
+
+import java.util.function.Function;
+
+public class FinalDemoFun {
+    public static void main(String[] args) {
+        String str = "赵丽颖,20";
+        change(str,
+                s -> s.split(",")[1].toString(),
+                s -> Integer.parseInt(s),
+                s -> s+=100
+                );
+    }
+    public static void change(String str, Function<String,String> fun1, Function<String,Integer> fun2, Function<Integer,Integer> fun3){
+        System.out.println(fun1.andThen(fun2).andThen(fun3).apply(str));
+    }
+}
