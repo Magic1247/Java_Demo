dbb2da8625842931971c0c2abd9a4064de077fe2
Huaiguang <1533505111@qq.com>
2021-08-01 23:28:09 +0800

    NetStudy0801

---
+++ src/com/huaiguang/NetStudy/BSSever.java
@@ -0,0 +1,31 @@
+package com.huaiguang.NetStudy;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class BSSever {
+    public static void main(String[] args) throws IOException {
+        ServerSocket sever = new ServerSocket(8888);
+        while (true){
+            Socket socket = sever.accept();
+            InputStream is = socket.getInputStream();
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            String s = br.readLine();
+            String path = s.split(" ")[1].substring(1);
+            System.out.println("读取完毕"+path);
+            FileInputStream fis = new FileInputStream(path);
+            byte[] bytes1= new byte[1024];
+            int len1;
+            OutputStream os = socket.getOutputStream();
+            os.write("HTTP/1.1 200 OK\r\n".getBytes());
+            os.write("Content-Type: text/html;charset=utf-8\r\n".getBytes());
+            os.write("\r\n".getBytes());
+            while ((len1 = fis.read(bytes1))!=-1){
+               os.write(bytes1,0,len1);
+            }
+            System.out.println("0");
+
+        }
+    }
+}
--- src/com/huaiguang/NetStudy/FileUploadSever.java
+++ src/com/huaiguang/NetStudy/FileUploadSever.java
@@ -1,25 +1,40 @@
 package com.huaiguang.NetStudy;
 
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Random;
 
 public class FileUploadSever {
     public static void main(String[] args) throws IOException {
         ServerSocket sever = new ServerSocket(8888);
-        Socket accept = sever.accept();
-        FileOutputStream fos = new FileOutputStream("D:\\test\\uploadfile\\testuploadfile01.txt");
-        InputStream is = accept.getInputStream();
-        byte[] bytes = new byte[1024];
-        int len = 0;
-        while ((len = is.read(bytes))!=-1){
-            fos.write(bytes,0,len);
+        while(true){
+            Socket accept = sever.accept();
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try{
+                        String name = "test"+System.currentTimeMillis()+(new Random().nextInt(99999))+".txt";
+                        FileOutputStream fos = new FileOutputStream("D:\\test\\uploadfile\\"+name);
+                        InputStream is = accept.getInputStream();
+                        byte[] bytes = new byte[1024];
+                        int len = 0;
+                        while ((len = is.read(bytes))!=-1){
+                            fos.write(bytes,0,len);
+                        }
+                        accept.getOutputStream().write("上传成功".getBytes());
+//            sever.close();
+                        fos.close();
+                        is.close();
+                    }catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
+
         }
-        accept.getOutputStream().write("上传成功".getBytes());
-        sever.close();
-        fos.close();
-        is.close();
     }
 }
