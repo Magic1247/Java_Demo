6ef13f7153313adc3d057e55fd4d78ee5b3024da
Huaiguang <1533505111@qq.com>
2021-08-02 00:14:33 +0800

    Junt

---
+++ src/com/huaiguang/Juit/Calc.java
@@ -0,0 +1,8 @@
+package com.huaiguang.Juit;
+
+public class Calc {
+    public int add(int a, int b){
+        int i = 3/0;
+        return a+b;
+    }
+}
---
+++ src/com/huaiguang/Juit/CalcTest.java
@@ -0,0 +1,11 @@
+package com.huaiguang.Juit;
+
+import org.junit.Test;
+
+public class CalcTest {
+    @Test
+    public void testcase01(){
+        Calc calc = new Calc();
+        int add = calc.add(1, 2);
+    }
+}
