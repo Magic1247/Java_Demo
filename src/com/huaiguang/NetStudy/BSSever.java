3b21d10cc7f4bc5b85c16b29d26f6d316b2bf06b
Huaiguang <1533505111@qq.com>
2021-08-02 00:01:56 +0800

    NetStudy0801

--- src/com/huaiguang/NetStudy/BSSever.java
+++ src/com/huaiguang/NetStudy/BSSever.java
@@ -10,21 +10,31 @@ public class BSSever {
         while (true){
             Socket socket = sever.accept();
             InputStream is = socket.getInputStream();
-            BufferedReader br = new BufferedReader(new InputStreamReader(is));
-            String s = br.readLine();
-            String path = s.split(" ")[1].substring(1);
-            System.out.println("读取完毕"+path);
-            FileInputStream fis = new FileInputStream(path);
-            byte[] bytes1= new byte[1024];
-            int len1;
-            OutputStream os = socket.getOutputStream();
-            os.write("HTTP/1.1 200 OK\r\n".getBytes());
-            os.write("Content-Type: text/html;charset=utf-8\r\n".getBytes());
-            os.write("\r\n".getBytes());
-            while ((len1 = fis.read(bytes1))!=-1){
-               os.write(bytes1,0,len1);
-            }
-            System.out.println("0");
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        BufferedReader br = new BufferedReader(new InputStreamReader(is));
+                        String s = br.readLine();
+                        String path = s.split(" ")[1].substring(1);
+                        System.out.println("读取完毕"+path);
+                        FileInputStream fis = new FileInputStream(path);
+                        byte[] bytes1= new byte[1024];
+                        int len1;
+                        OutputStream os = socket.getOutputStream();
+                        os.write("HTTP/1.1 200 OK\r\n".getBytes());
+                        os.write("Content-Type: text/html;charset=utf-8\r\n".getBytes());
+                        os.write("\r\n".getBytes());
+                        while ((len1 = fis.read(bytes1))!=-1){
+                            os.write(bytes1,0,len1);
+                        }
+                        System.out.println("0");
+                    }catch (IOException e){
+                        System.out.println(e);
+                    }
+
+                }
+            }).start();
 
         }
     }
